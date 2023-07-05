9231d1322ce9affb1147d9ded7aceb4c3f7ef566
Aymeric <aymeric.cretin@smartesting.com>
2023-07-05 08:36:59 +0200

    Reorganize files

---
+++ src/recording-stream/streamRecording.ts
@@ -0,0 +1,14 @@
+import {Recording} from "../types";
+import {sortRecord} from "../utils/recording/sortRecord";
+import {getDelay} from "../utils/recording/getDelay";
+import {sleep} from "../utils/sleep";
+
+export async function streamRecording(recording: Recording, callback: (message: string) => void) {
+    let previousMessage: string | null = null
+    for (const message of sortRecord(recording)) {
+        const delay = previousMessage ? getDelay(previousMessage, message) : 0;
+        await sleep(delay);
+        callback(message);
+        previousMessage = message
+    }
+}
---
+++ src/utils/recording/sortRecord.ts
@@ -0,0 +1,17 @@
+import {Recording} from "../../types";
+
+export function sortRecord(recording: Recording): string[] {
+    let contentInArray: string[] = recording.content.trim().split('\n')
+    const cleTri = (ligne: string): Date => {
+
+        const parts = ligne.split(',')
+        if (ligne !== contentInArray[0]) {
+            return new Date(`${parts[8]} ${parts[9]}`);
+        } else {
+            const partsFirstLine = contentInArray[0].split(',')
+            return new Date(`${partsFirstLine[8]} ${partsFirstLine[9]}`);
+        }
+    };
+
+    return [...contentInArray].sort((a, b) => cleTri(a).getTime() - cleTri(b).getTime())
+}
