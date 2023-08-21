1ede5c5988a1b982d77bc5147b2064fd1738d5ce
Helchida <morgan.leclercq89@gmail.com>
2023-08-21 16:26:59 +0200

    add function sort record by date csv

--- src/recording-stream/streamRecording.ts
+++ src/recording-stream/streamRecording.ts
@@ -1,17 +1,18 @@
 import {MessageCallback} from '../types'
-import {sortRecordByDate} from '../utils/recording/sortRecordByDate'
+import {sortRecordByDate, sortRecordByDateCSV} from '../utils/recording/sortRecordByDate'
 import {getDelay} from '../utils/recording/getDelay'
 import {sleep} from '../utils/sleep'
 
 export async function streamRecording(
     recordingContent: string,
     callback: MessageCallback,
-    speed: number | null
+    speed: number | undefined
 ): Promise<void> {
+
     let previousMessage: string | null = null
-    for (const message of sortRecordByDate(recordingContent)) {
+    for (const message of sortRecordByDateCSV(recordingContent)) {
         const delay = previousMessage ? getDelay(previousMessage, message) : 0
-        if (speed == null || speed <= 0) {
+        if (speed == undefined || speed <= 0) {
             await sleep(delay)
         } else {
             await sleep(delay / speed)
--- src/utils/recording/sortRecordByDate.ts
+++ src/utils/recording/sortRecordByDate.ts
@@ -10,3 +10,16 @@ export function sortRecordByDate(recordingContent: string): string[] {
     .filter((message) => !isNaN(sortKey(message).getTime()))
   return messages.sort((a, b) => sortKey(a).getTime() - sortKey(b).getTime())
 }
+
+export function sortRecordByDateCSV(recordingContent: string): string[] {
+  const sortKey = (line: string): number => {
+    const parts = line.split(',')
+    return parseInt(parts[0])
+  }
+
+  const messages = recordingContent
+      .trim()
+      .split('\n')
+      .filter((message) => !isNaN(sortKey(message)))
+  return messages.sort((a, b) => sortKey(a) - sortKey(b))
+}
